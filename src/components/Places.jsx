cc421e747ac1a2410e17d194fc4727fcfc986bbe
Endrit Basha <bashaditi@gmail.com>
2023-09-09 23:07:45 -0400

    changed layout of places page

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -8,7 +8,8 @@ const url = 'http://localhost:5000/api/places';
 const Places = () => {
   const [places, setPlaces] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
   //fetch places when page loads from database
   useEffect(() => {
     fetch('http://localhost:5000/api/places')
@@ -16,6 +17,7 @@ const Places = () => {
       .then((data) => {
         console.log("RESPONSE FROM DB", data);
         setPlaces(data);
+        setFilteredPlaces(data);
       });
   }, []);
 
@@ -27,30 +29,40 @@ const Places = () => {
   };
 
   const handleAddPlace = (newPlace) => {
-    console.log("newPlace", newPlace)
-    // Add a new place to the list make the post call to the database
+    console.log("newPlace", newPlace);
+
+    // Add a new place to the list and make a POST call to the database
     fetch('http://127.0.0.1:5000/api/places', { 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(newPlace),
-    }).then((res) => res.json())
+    })
+      .then((res) => res.json())
       .then((data) => {
         console.log("RESPONSE FROM DB", data);
         setPlaces([...places, data]);
+        setFilteredPlaces([...filteredPlaces, data]);
       });
-    //  
-    //
-    //
-
-    setPlaces([...places, newPlace]);
   };
 
   const handleDeletePlace = (index) => {
-    // Delete a place from the list
     const updatedPlaces = places.filter((place, i) => i !== index);
     setPlaces(updatedPlaces);
+    setFilteredPlaces(updatedPlaces);
+  };
+  const handleCategoryFilter = (category) => {
+    // Filter places by selected category
+    if (category === 'All') {
+      setFilteredPlaces(places);
+    } else {
+      const filtered = places.filter((place) =>
+        place.selectedCategories.includes(category)
+      );
+      setFilteredPlaces(filtered);
+    }
+    setSelectedCategories([category]);
   };
 
     // Group places by category
@@ -61,40 +73,66 @@ const Places = () => {
       acc[place.category].push(place);
       return acc;
     }, {});
-
-  return (
-    <div>
-      <Navbar />
-      <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center p-8">
-        <h1 className="text-4xl text-red-500 font-semibold mb-4">Our Favorite Places</h1>
-        <button onClick={handleModalOpen} className="bg-red-500 text-white py-2 px-6 rounded-full text-lg hover:bg-red-600 mb-4">
-          Add New Place
-        </button>
-        <AddNewPlaceModal isOpen={isModalOpen} onClose={handleModalClose} onSuccess={handleAddPlace} />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {/* Loop through categorized places */}
-        {Object.keys(categorizedPlaces).map((category) => (
-          <div key={category} className="mb-8">
-            <h2 className="text-2xl text-white font-semibold mb-2">{category}</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {categorizedPlaces[category].map((place, index) => (
-                <Place
-                  key={index}
-                  name={place.name}
-                  location={place.location}
-                  selectedCategories={place.selectedCategories}
-                  rating={place.rating}
-                  reviews={place.reviews}
-                  onDelete={() => handleDeletePlace(index)}
-                />
-              ))}
-            </div>
+    return (
+      <div>
+        <Navbar />
+        <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center p-8">
+          <div className="flex flex-col items-center">
+            <h1 className="text-4xl text-red-500 font-semibold mb-4">
+              Our Favorite Places
+            </h1>
+            <button
+              onClick={handleModalOpen}
+              className="bg-red-500 text-white py-2 px-6 rounded-full text-lg hover:bg-red-600 mb-4"
+            >
+              Add New Place
+            </button>
           </div>
-        ))}
+          <AddNewPlaceModal
+            isOpen={isModalOpen}
+            onClose={handleModalClose}
+            onSuccess={handleAddPlace}
+          />
+          <div className="flex mb-4">
+            <button
+              onClick={() => handleCategoryFilter('All')}
+              className={`${
+                selectedCategories.includes('All')
+                  ? 'bg-red-500 text-white'
+                  : 'bg-gray-700 text-gray-300'
+              } py-1 px-4 rounded-full text-lg mr-4`}
+            >
+              All
+            </button>
+            {/* Replace with your logic to display available categories */}
+            {/* Example:
+            <button
+              onClick={() => handleCategoryFilter('Category1')}
+              className={`${
+                selectedCategories.includes('Category1')
+                  ? 'bg-red-500 text-white'
+                  : 'bg-gray-700 text-gray-300'
+              } py-1 px-4 rounded-full text-lg mr-4`}
+            >
+              Category1
+            </button>
+            */}
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {filteredPlaces.map((place, index) => (
+              <Place
+                key={index}
+                name={place.name}
+                location={place.location}
+                selectedCategories={place.selectedCategories}
+                rating={place.rating}
+                reviews={place.reviews}
+                onDelete={() => handleDeletePlace(index)}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  };
 export default Places;
